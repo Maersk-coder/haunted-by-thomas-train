c5722d825807874c20962d202eb1f93d89c6b93b
Maersk-coder <Thomasmaersk@hotmail.com>
2024-10-21 10:40:57 +0200

    Simple ændringer

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
+function Create_Thomas_Tog () {
+    Thomas_Tog = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Enemy)
+}
+sprites.onOverlap(SpriteKind.Enemy, SpriteKind.Player, function (sprite, otherSprite) {
+    game.setGameOverEffect(false, effects.splatter)
+})
 function Change_level (Level_nummer: string) {
     let Level_number = 0
     if (Level_number == 1) {
@@ -8,5 +31,28 @@ function Change_level (Level_nummer: string) {
         tiles.setCurrentTilemap(tilemap`level4`)
     }
 }
+let Thomas_Tog: Sprite = null
 tiles.setCurrentTilemap(tilemap`level4`)
 let Current_level = 1
+let Hero = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+game.onUpdate(function () {
+    controller.moveSprite(Hero, 100, 100)
+    scene.cameraFollowSprite(Hero)
+})
