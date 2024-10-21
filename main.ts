0d16a1f412f4eb125e8e33b05d6a1dad37cf100d
Maersk-coder <Thomasmaersk@hotmail.com>
2024-10-21 10:31:50 +0200

    Change level mv

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
-game.gameOver(true)
+function Change_level (Level_nummer: string) {
+    let Level_number = 0
+    if (Level_number == 1) {
+        tiles.setCurrentTilemap(tilemap`level4`)
+    } else if (Level_number == 2) {
+        tiles.setCurrentTilemap(tilemap`level4`)
+    } else if (Level_number == 3) {
+        tiles.setCurrentTilemap(tilemap`level4`)
+    }
+}
+tiles.setCurrentTilemap(tilemap`level4`)
+let Current_level = 1
