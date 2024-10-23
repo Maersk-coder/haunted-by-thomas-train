b36c81325ac14321125a4d1dd0b1418aaeeaa752
Kriller123 <kristian.lillevang103@gmail.com>
2024-10-23 12:55:05 +0200

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -25,6 +25,20 @@ function Change_level (Level_nummer: string) {
 sprites.onDestroyed(SpriteKind.Food, function (sprite) {
     Olie()
 })
+function startGame () {
+    info.setScore(0)
+    Food = sprites.create(assets.image`Olie`, SpriteKind.Food)
+    tiles.placeOnRandomTile(Food, assets.tile`Togspor lodret`)
+    Sir_Topham_Hatt()
+    Level_number = 1
+    Change_level("1")
+    if (info.score() == 1) {
+        Change_level("2")
+    }
+    tiles.placeOnRandomTile(Hero, assets.tile`Togspor vandret`)
+    Create_Thomas_Tog()
+    Randombevægelse_Thomas()
+}
 function Adgang_til_bevægelse_Thomas (Enemysprite: string) {
     let direction: string[] = []
     if (!(Thomas_Tog.tileKindAt(TileDirection.Left, sprites.castle.tileGrass1))) {
@@ -66,23 +80,33 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSpr
 function Create_Thomas_Tog () {
     Thomas_Tog = sprites.create(assets.image`Thomas`, SpriteKind.Enemy)
 }
-let Thomas_Tog: Sprite = null
-let Randombevægelse = ""
+let Food: Sprite = null
 let Hero: Sprite = null
 let Level_number = 0
-let Food: Sprite = null
-info.setScore(0)
-Food = sprites.create(assets.image`Olie`, SpriteKind.Food)
-tiles.placeOnRandomTile(Food, assets.tile`Togspor lodret`)
-Sir_Topham_Hatt()
-Level_number = 1
-Change_level("1")
-if (info.score() == 1) {
-    Change_level("2")
-}
-tiles.placeOnRandomTile(Hero, assets.tile`Togspor vandret`)
-Create_Thomas_Tog()
-Randombevægelse_Thomas()
+let Thomas_Tog: Sprite = null
+let Randombevægelse = ""
+music.play(music.stringPlayable("E B C5 A B G A F ", 100), music.PlaybackMode.LoopingInBackground)
+let isOnTitleScreen = true
+// Opret en variabel til baggrunden
+let titleBackground = image.create(160, 120)
+// Tegn et simpelt tog (der minder om Thomas Toget)
+// Lys blå baggrund for himlen
+titleBackground.fill(1)
+// Indstil baggrunden som title screen
+scene.setBackgroundImage(titleBackground)
+// Viser title screen tekst
+game.splash("Haunted by Thomas Train", "Press \"A\" to start")
+titleBackground.fill(8)
+game.splash("For at vinde spillet")
+titleBackground.fill(15)
+game.splash("Indsaml alt olie")
+titleBackground.fill(2)
+game.splash("og overlev de 3 levels")
+titleBackground.fill(10)
+game.splash("Held og lykke")
+game.splash("Spillet starter nu!")
+// Start spillet
+startGame()
 game.onUpdate(function () {
     controller.moveSprite(Hero, 75, 75)
     scene.cameraFollowSprite(Hero)
